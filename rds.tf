829187ccad3f6e290ed50275f9368a3d37ac189e
evilfeeh <felipegoncalvesdsantos@gmail.com>
2024-09-14 10:29:02 -0300

    feat?updating terraform and changing the password

--- rds.tf
+++ rds.tf
@@ -1,26 +1,27 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "localstack"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "default_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "default vpc"
+    Name = "default-vpc"
   }
 }
 
 resource "aws_subnet" "public_subnet_a" {
-  vpc_id            = aws_vpc.default_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.default_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_b" {
-  vpc_id            = aws_vpc.default_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.default_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet_a" {
@@ -35,14 +36,83 @@ resource "aws_subnet" "private_subnet_b" {
   availability_zone = "us-east-1b"
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default_vpc.id
+  tags = {
+    Name = "default-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.default_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a_association" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_b_association" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.default_vpc.id
+  name   = "rds-sg"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+# RDS Database Instance
 resource "aws_db_instance" "database" {
-  allocated_storage     = 10
-  max_allocated_storage = 20
-  db_name               = "self-service-totem"
-  engine                = "mysql"
-  engine_version        = "5.7"
-  instance_class        = "db.t3.micro"
-  username              = "admin"
-  password              = "e:M4uT9BxRGj>(5k6+3}pw"
-  skip_final_snapshot   = true
+  identifier             = var.identifier
+  allocated_storage      = var.allocated_storage
+  max_allocated_storage  = var.max_allocated_storage
+  db_name                = var.db_name
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  username               = var.username
+  password               = var.password
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
 }
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "identifier" {
+  default = "sst-db"
+  type    = string
+}
+
+variable "allocated_storage" {
+  default = 5
+  type    = number
+}
+
+variable "max_allocated_storage" {
+  default = 10
+type    = number
+}
+
+variable "db_name" {
+  default = "self_service_totem"
+  type    = string
+}
+
+variable "engine" {
+  default = "mysql"
+  type    = string
+}
+
+variable "engine_version" {
+  default = "5.7"
+  type    = string
+}
+
+variable "instance_class" {
+  default = "db.t3.micro"
+  type    = string
+}
+
+variable "username" {
+  default = "admin"
+  type    = string
+}
+
+variable "password" {
+  default = "none"
+  type    = string
+}
